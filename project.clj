5baa9afb5d1de5c223d700c8f91007b82028d624
Duncan M. McGreggor <oubiwann@gmail.com>
2015-09-02 09:15:30 -0400

    Updated to latest version of Clojure and clj deps.

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
-(defproject cljnode "0.1.2"
+(defproject cljnode "0.1.3"
   :jvm-opts ["-Ddev_env=dev"
              "-Dnode=cljnode@127.0.0.1"
              "-Dmbox=mboxname"
              "-Dcookie=nocookie"
              "-Depmd_port=15000"]
   :description "Clojure + LFE"
-  :url "http://vonmo.com/projects/cljnode"
+  :url "https://github.com/lfex/lfecljapp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :resource-paths [ ]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.2.4"]
-                 [org.slf4j/slf4j-log4j12 "1.7.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/slf4j-log4j12 "1.7.12"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jmdk/jmxtools
